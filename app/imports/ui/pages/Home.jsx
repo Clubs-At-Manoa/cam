89abb5083511bf277aa43132d033c910688c46ce
Chester Leoso <chester.n.leoso@gmail.com>
2024-05-08 20:20:08 -1000

    Updated the columns size.

--- app/imports/ui/pages/Home.jsx
+++ app/imports/ui/pages/Home.jsx
@@ -8,13 +8,12 @@ const Home = () => (
       <Col xs={4}>
         <Image roundedCircle src="/images/meteor-logo.png" width="150px" />
       </Col>
-      <Col xs={3} className="d-flex flex-column justify-content-center">
+      <Col xs={4} className="d-flex flex-column justify-content-center">
         <h1>Welcome to Clubs at Manoa</h1>
         <p>Lets get started!</p>
         <p>The University of Hawaiis Board of Regents BOR acknowledges through Chapter 7 of its policies that robust student life programs should foster critical thinking, problem-solving, decision-making, and leadership skill development </p>
         <p>to prepare students to become informed, responsible citizens who engage in and continue to make contributions for the betterment of humanity and the improvement of our communities RP 7.201.</p>
       </Col>
-
     </Row>
   </Container>
 );
